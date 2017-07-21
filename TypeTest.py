24554846683d202ad0d0b68a54abd3335dae3ead
Line77 <josevelarde00@gmail.com>
2017-07-21 16:01:52 -0300

    Update TypeTest.py

--- TypeTest.py
+++ TypeTest.py
@@ -1,133 +1,233 @@
-from tkinter import *
-from tkinter import ttk, font
-from difflib import SequenceMatcher
+#!/usr/bin/env python3
+import tkinter as tk
+from tkinter import ttk
+from tkinter.font import Font
+import random
+import sys
 
 class TypeTester:
-    def __init__(self, master):
-        self.master = master
-        master.title("TypeTester")
+    def __init__(self):
+        self.window = self.create_window()
 
-        # Get test text, single line of a text file
+        self.my_font = Font(family = "Verdana", size ="12", weight = "bold")
         self.random_line = ""
-        self.get_phrase("phrases.txt")
-
-        # Define start time variable for pwm calculation
-        self.start_time = 0
-
-        # Font for all widgets
-        self.all_font = font.Font(family = "Verdana", size ="11", weight = "bold")
-
-        # Create text widget
-        self.text_frame = Text(master, width=50, height=10, font=self.all_font, wrap="word",background = "yellow")
-        self.text_frame.insert("1.0", self.random_line)
-
-        # Define text tags, each one modifies text to show current state
-        self.text_frame.tag_configure("correct", foreground="gray")
-        self.text_frame.tag_configure("wrong", foreground="red")
-        self.text_frame.tag_configure("next", background="lightblue")
-
-        # Create text entry widget
-        self.text_input = StringVar()
-        self.entry_frame = ttk.Entry(master, width=50, font = self.all_font)
-
-        # Create label widget to show WPM
-        self.wpm = DoubleVar()
-        self.wpm_title = Label(master, font= self.all_font, text = "WPM", anchor = "ne")    #, background = "blue")
-        self.wpm_number = Label(master, font= self.all_font, textvariable= self.wpm, anchor = "nw",width = 7)#,background = "orange")
+        self.wpm_value = tk.DoubleVar()
+        self.average_value = tk.DoubleVar()
         
-        # Arrange widgets, layout
-        master.grid_rowconfigure(3, weight=1)
-        master.grid_columnconfigure(0, weight=1)
-        self.text_frame.grid(column=0, row=1, columnspan=2, sticky = "ew")
-        self.entry_frame.grid(column=0, row=0, columnspan=2, sticky = "new")
-        self.wpm_title.grid(column=0, row=3, sticky = "nsew")
-        self.wpm_number.grid(column=1, row=3, sticky = "nsew")
+        # Create containers
+        self.text_frame, self.entry_frame, self.info_frame = self.create_frames(self.window)
+        # Create widgets
+        self.text_widget = self.create_text_widget()
+        self.entry_widget = self.create_entry_widget()
+        self.pwm_title_label, self.pwm_value_label = self.create_info_widgets("WPM", self.wpm_value)
+        self.average_title_label, self.average_value_label = self.create_info_widgets("Average", self.average_value)
 
+        self.set_tags()
+        self.initialize_all()
+        self.set_callbacks()
         
-        # Focus text entry box on start
-        self.entry_frame.focus()
-        # Call text update method each time a key is released, event object is passed
-        self.entry_frame.bind('<KeyRelease>', self.read_entry)
+        self.load_random_line()
+        self.entry_widget.focus()
         
-    def get_phrase(self, filename):
-        """
-        Gets "random" line from a text
-        """
-        with open(filename, "rt",encoding='utf-8') as text_file:
-            self.random_line = text_file.readline()
+    def create_window(self):
+        window = tk.Tk()
+        window.title("TypeTester")
+        window.grid_columnconfigure(0, weight=1)
+        window.grid_rowconfigure(1, weight=1)
+        window.resizable(True, True)                # expand hor/ver       
+        
+        return window
+        
+    def create_frames(self, window):
+        text_frame = tk.Frame(window)                    
+        text_frame.grid(row=1, column=0, sticky = "news")
+        #text_frame.columnconfigure(0, weight=1)
+        #text_frame.rowconfigure(1, weight=1)
+        
+        entry_frame = tk.Frame(window)                    
+        entry_frame.grid(row=0, column=0, sticky = "new")
+        
+        info_frame = tk.Frame(window)                    
+        info_frame.grid(row=2, column=0)
+        
+        return text_frame, entry_frame, info_frame
 
-    def read_entry(self, event):
-        """
-        Gets text from entry widget and compares with test text
-        """
-        # Gets text on entry widget
-        self.text_input = self.entry_frame.get()
+    def create_text_widget(self):
+        text_widget = tk.Text(self.text_frame, width=50, height=10, font=self.my_font, wrap="word")
+        text_widget.pack(side="top", fill = "both", expand = True)
+        #text_widget.grid(row=0, column=0, sticky = "news")
+        
+        return text_widget
 
-        # seq contains: seq.a= len(not-matched characters), seq.size=len(matched chars)
-        seq = SequenceMatcher(None,self.text_input,self.random_line).find_longest_match(0,len(self.text_input),0,len(self.random_line))
+    def create_entry_widget(self):
+        entry_widget = ttk.Entry(self.entry_frame, width=50, font = self.my_font)          
+        entry_widget.pack(side="top", fill = "both")
+        #entry_widget.grid(row=0, column=0)
 
+        return entry_widget
+
+    def create_info_widgets(self, title ,value):
+        title_label = tk.Label(self.info_frame, font= self.my_font, text = title, anchor = "ne")
+        value_label = tk.Label(self.info_frame, font= self.my_font, textvariable=value, anchor = "nw",width = 7)
+        #title_label.grid(row=0, column=0)
+        #value_label.grid(row=0, column=1)
+        title_label.pack(side="left")
+        value_label.pack(side="left")
+
+        return title_label, value_label
+        
+    def set_tags(self):
+        self.text_widget.tag_configure("correct", foreground="gray")
+        self.text_widget.tag_configure("wrong", foreground="red")
+        self.text_widget.tag_configure("next", background="lightblue")
+
+    def initialize_all(self, clear_text = False):
+        self.test_end = False
+        self.test_start = False
+        self.entry_widget.delete(0, "end")
+        self.clear_text_widget()
+        self.text_widget.tag_add("next", "1.0")
+        self.match_end_index = 0
+        self.wpm_value.set(0)
+        self.average_value.set(self.get_average_speed("speed_records.txt"))
+        if clear_text:
+            self.text_widget.delete("1.0", "end")
+        
+    def set_callbacks(self):
+        if not self.test_start:
+            self.entry_widget.bind('<KeyPress>', self.set_start_time)
+        self.entry_widget.bind('<KeyRelease>', self.handle_entry)
+            
+    def load_random_line(self):
+        self.random_line = self.get_random_line("phrases.txt")
+        self.text_widget.config(state = "normal")
+        self.text_widget.delete("1.0", "end")
+        self.text_widget.insert("1.0", self.random_line)
+        self.text_widget.config(state = "disabled")
+        self.text_widget.tag_add("next", "1.0")
+        
+    def get_random_line(self, filename):
+        with open(filename, "rt", encoding="utf-8") as text_file:
+            random_line = next(text_file)
+            for num, line in enumerate(text_file):
+                if random.randrange(num + 2):
+                    continue
+                random_line = line
+                
+        return random_line
+        
+    def clear_text_widget(self):
+        self.text_widget.tag_remove("correct", "1.0", "end")
+        self.text_widget.tag_remove("wrong", "1.0", "end")
+        self.text_widget.tag_remove("next", "1.0", "end")
+        self.text_widget.configure(bg="white")
+
+    def set_start_time(self,event):
+        # Define start time: only if character is correct and is the first of entry widget text
+        # Called on a one time key press event.
+        if (event.char == self.random_line[0] and not self.entry_widget.get() and not self.test_start):
+            self.start_time = event.time
+            self.test_start = True
+
+    def handle_entry(self, event):
+        # Called on key release event
+        #print(event.keycode)                       # keycodes change on different OS
+        if event.keycode == 27:                     # Escape key event
+            self.initialize_all(True)
+            self.load_random_line()
+        elif event.keycode == 9:                    # Tab key event
+            self.initialize_all()
+        elif event.char or (event.keycode == 8):    # Char or Backspace key event
+            self.handle_text_widget(event)
+        else:                                       # Shift key event, other.
+            pass
+
+    def handle_text_widget(self,event):
+        if self.test_end:
+            return None
+        self.text_input = self.entry_widget.get()
+        # Get len(not_matched_ chars) and len(matched_chars)
+        not_matched, matched_chars = self.matcher(self.text_input, self.random_line)
+        
         # Markers for tags
-        self.text_frame.mark_set("matchEnd",    "1.{}".format(seq.size))
-        self.text_frame.mark_set("wrongStart",  "1.{}".format(seq.size))
-        self.text_frame.mark_set("wrongEnd",    "1.{}".format(len(self.text_input)))
-        self.text_frame.mark_set("nextChar",    "1.{}".format(seq.size))
+        self.text_widget.mark_set("matchEnd",    "1.{}".format(matched_chars))
+        self.text_widget.mark_set("wrongStart",  "1.{}".format(matched_chars))
+        self.text_widget.mark_set("wrongEnd",    "1.{}".format(len(self.text_input)))
+        self.text_widget.mark_set("nextChar",    "1.{}".format(matched_chars))
 
         # Update tag: completely remove tag and insert new(w updated index)
         # Update tag: "next" char to input(light blue)
-        self.text_frame.tag_remove("next", "1.0", "end")
-        self.text_frame.tag_add("next", "nextChar")
+        self.text_widget.tag_remove("next", "1.0", "end")
+        self.text_widget.tag_add("next", "nextChar")
 
-        # Define start time: only if character is correct and is the first of entry widget text
-        # != "" by-passes shift and other key presses, this way "start_time" is defined when
-        # the first correct character key is released
-        # Catch index out of range error
-        try:
-            if ((event.char != "") and (seq .a == 0) and
-                    (self.text_input is self.text_input[0]) and
-                    (self.text_input[0] == self.random_line[0])):
-                self.start_time = event.time
-        except:
-            pass
+        # Test finished successfully, save record to file, set all to correct.
+        if matched_chars == (len(self.random_line)-1):
+            self.write_record("speed_records.txt", self.wpm_value.get())
+            self.clear_text_widget()
+            self.text_widget.tag_add("correct", "1.0", "end")
+            self.average_value.set(self.get_average_speed("speed_records.txt"))
+            self.test_end = True
 
-        # Char is correct: no wrong chars on start, text is the same so far.
-        if seq.a == 0 and self.text_input == self.random_line[0:seq.size]:
+        # Tag correct characters (if any)
+        if matched_chars >=0 and not self.test_end:
             # Remove "wrong" tag if any, and set text widget background to white
-            self.text_frame.tag_remove("wrong", "1.0", "end")
-            self.text_frame.configure(bg="white")
+            self.text_widget.tag_remove("wrong", "1.0", "end")
+            self.text_widget.configure(bg="white")
 
-            # Update tag: good characters so far(gray)
-            self.text_frame.tag_remove("correct", "1.0", "end")
-            self.text_frame.tag_add("correct", "1.0", "matchEnd")
+            # Update tag: correct characters so far
+            self.text_widget.tag_remove("correct", "1.0", "end")
+            self.text_widget.tag_add("correct", "1.0", "matchEnd")
 
-            # Calculation for wpm, catch ZeroDivisionError
-            try:
-                self.wpm.set(round((seq.size)/(5*(event.time-self.start_time)/(1000*60)),2))
-            except:
-                pass
-        # Char is/are wrong: at start(or after text) doesn't match, or
-        # characters matched to (seq.size) then didn't anymore
-        if seq.a >0 or seq.b>0 or (seq.size != len(self.text_input)):
-            # text widget background to pink
-            self.text_frame.configure(bg="pink")
+            # Calculation for wpm
+            if event.char and not not_matched and self.test_start:
+                self.wpm_value.set(round((matched_chars)/(5*(event.time-self.start_time)/(1000*60)),2))
+                
+            # Tag incorrect characters
+            if not_matched:
+                # text widget background to pink
+                self.text_widget.configure(bg="pink")
 
-            # Don't tag next char to type
-            self.text_frame.tag_remove("next", "1.0", "end")
-            if seq.size < len(self.text_input):
-                if seq.a == 0 and self.text_input[0:seq.size] == self.random_line[0:seq.size]:
-                    # Hard-coded
-                    # If a key is pressed but not released immediately, more than one character will be 
-                    # present on entry box, but only one call to this method will be executed.
-                    # If this key is a correct input, it will not have a "correct tag",
-                    # will be black instead of gray.
-                    # This extra if statement fix that.
-                    self.text_frame.tag_remove("correct", "1.0", "end")
-                    self.text_frame.tag_add("correct", "1.0", "1.{}".format(seq.size+1))
+                # Don't tag next char to type
+                self.text_widget.tag_remove("next", "1.0", "end")
 
                 # Update tag: wrong characters after the correct ones.
-                self.text_frame.tag_remove("wrong", "1.0", "end")
-                self.text_frame.tag_add("wrong", "wrongStart", "wrongEnd")
+                self.text_widget.tag_remove("wrong", "1.0", "end")
+                self.text_widget.tag_add("wrong", "wrongStart", "wrongEnd")
 
+    def matcher(self, string_a, string_b):
+        """
+        string_a : input string
+        string_b : full string to match to
+        return len(not_matched_chars, matched_chars)
+        """
+        
+        if string_a == string_b[0:len(string_a)]:
+            matched = len(string_a)
+        else:
+            matched = self.match_end_index
+            for i,char in enumerate(string_a[self.match_end_index+1:len(string_a)]):
+                if string_a[0:self.match_end_index+1+i] == string_b[0:self.match_end_index+1+i]:
+                    matched += 1
+                    
+        not_matched = len(string_a)- matched
+        self.match_end_index = matched
+        
+        return not_matched, matched
 
-root = Tk()
-my_gui = TypeTester(root)
-root.mainloop()
+    def write_record(self, filename, wpm_value):
+        with open(filename, "a") as record_file:
+            record_file.write(str(wpm_value)+"\n")
+
+    def get_average_speed(self, filename):
+        average = 0
+        count = 0
+        with open(filename, "rt") as record_file:
+            for value in record_file:
+                average += float(value)
+                count += 1
+            average = average/count
+        return round(average,2)
+        
+program = TypeTester()
+
+program.window.mainloop()
